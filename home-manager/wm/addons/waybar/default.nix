aede23a16126e73ef2493b5cc60a7aa792143c43
dx5 <247799176+InDexter10@users.noreply.github.com>
2026-05-27 15:05:23 +0300

    fixed

--- home-manager/wm/addons/waybar/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  home.packages = [ pkgs.waybar ];
-  xdg.configFile."waybar/config".source = ./config.jsonc;
-  xdg.configFile."waybar/style.css".source = ./style.css;
-  xdg.configFile."waybar/tw".source = ./tw;
-}
